ab8487b6488862bd991c74b8c609de046bfcda51
Nicko <quiamconicko@gmail.com>
2021-09-29 01:06:39 +0800

    update in homepage

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,27 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Page Home</h1>
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Nicko Quiamco</q-item-label>
+          <q-item-label caption>
+            Caloocan City
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+
+    </q-card>
   </q-page>
 </template>
 
@@ -11,3 +32,10 @@ export default defineComponent({
   name: 'pageHome'
 })
 </script>
+<style lang="scss" scoped>
+.card-post{
+  .q-img{
+    min-height: 200px;
+  }
+}
+</style>
