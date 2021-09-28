2a2ccc89cfe4e1e6728d2de214bcd6ac3949cdd0
Nicko <quiamconicko@gmail.com>
2021-09-28 23:30:47 +0800

    main layout finish

--- quasar.conf.js
+++ quasar.conf.js
@@ -44,7 +44,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
 
@@ -72,7 +72,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 100,
       open: true // opens browser window automatically
     },
 
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,27 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white text-blue-grey-10" bordered>
-      <q-toolbar>
+      <q-toolbar class="constrain">
+        <q-btn
+          icon="eva-camera-outline"
+          class="large-screen"
+          size="18px"
+          to="/camera"
+          flat
+          round
+          dense />
+        <q-separator class="large-screen" vertical spaced />
         <q-toolbar-title class="text-grand-hotel text-bold">
           Quasagram
         </q-toolbar-title>
+        <q-btn
+          icon="eva-home-outline"
+          class="large-screen q-mr-sm"
+          size="18px"
+          to="/"
+          flat
+          round
+          dense />
 
       </q-toolbar>
     </q-header>
@@ -13,7 +30,7 @@
       <router-view />
     </q-page-container>
 
-    <q-footer class="bg-white" bordered >
+    <q-footer class="bg-white small-screen" bordered >
       <q-tabs class="text-blue-grey-10" active-color="primary" indicator-color="transparent">
         <q-route-tab to="/" icon="eva-home-outline" />
         <q-route-tab to="/camera" icon="eva-camera-outline" />
@@ -35,8 +52,15 @@ export default defineComponent({
 })
 </script>
 <style lang="scss" scoped>
+.q-toolbar{
+  @media (min-width: $breakpoint-sm-min){
+    height: 70px;
+  }
+}
 .q-toolbar__title{
-  text-align: center;
+  @media (max-width: $breakpoint-xs-max){
+    text-align: center;
+  }
   font-size: 30px;
 }
 </style>
