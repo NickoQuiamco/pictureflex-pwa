e4de28985df56f6e2ba92b0de5f3d477f8b316b1
Nicko <quiamconicko@gmail.com>
2021-10-03 10:20:02 +0800

    camera page UI

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,6 +1,58 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Page Camera</h1>
+  <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <div class="camera-frame q-pa-md">
+          <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg">
+        </div>
+        <div class="text-center q-pa-md">
+          <q-btn
+            icon="eva-camera" 
+            color="grey-10"
+            size="lg"
+            round
+            />
+        </div>
+        <div class="row justify-center q-ma-md">
+          <q-input class="col col-sm-6" label="Caption" dense />
+        </div>
+        <div class="row justify-center q-ma-md">
+          <q-input
+            class="col col-sm-6"
+            label="Location"
+            dense>
+            <template v-slot:append>
+              <q-btn round dense flat icon="eva-navigation-2-outline" />
+            </template>
+          </q-input>
+        </div>
+        <div class="row justify-center q-mt-lg">
+          <q-btn
+            unelevated
+            rounded
+            color="primary"
+            label="Post Image" />
+        </div>
+      </div>
+      <div class="col-4 large-screen">
+        <q-item class="fixed">
+            <!-- {{ props_data }} -->
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">Krono Lyon</q-item-label>
+            <q-item-label caption>
+              Nicko Quiamco
+              <!-- {{props_data.location}} -->
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -11,3 +63,9 @@ export default defineComponent({
   name: 'PageCamera'
 })
 </script>
+<style lang="scss" scoped>
+.camera-frame{
+  border: 2px solid $grey-10;
+  border-radius: 10px;
+}
+</style>
