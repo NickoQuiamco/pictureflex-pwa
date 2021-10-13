52987f882da5dcc14812138653855547022929a8
Nicko <quiamconicko@gmail.com>
2021-10-14 03:12:00 +0800

    caching strategies

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -7,10 +7,44 @@
 /* 
     Dependecies
 */
-import { precacheAndRoute } from 'workbox-precaching'
+    import { precacheAndRoute } from 'workbox-precaching'
+    import { registerRoute } from 'workbox-routing';
+    import { CacheFirst, StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies';
+    import { ExpirationPlugin } from 'workbox-expiration';
+    import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 /* 
     Config
 */
 // Use with precache injection
-precacheAndRoute(self.__WB_MANIFEST)
+    precacheAndRoute(self.__WB_MANIFEST)
+
+/* 
+    Caching Strategies
+*/
+    registerRoute(
+        ({ url }) =>url.host.startsWith('fonts.g'),
+        new CacheFirst({
+            cacheName: 'google-fonts',
+            plugins: [
+                new ExpirationPlugin({
+                    maxEntries: 30,
+                }),
+                new CacheableResponsePlugin({
+                    statuses: [0, 200]
+                }),
+            ],
+          })
+    );
+
+    registerRoute(
+        ({url}) => url.pathname.startsWith('/posts'),
+        new NetworkFirst({
+            cacheName:'posts-requests'
+        })
+    );
+
+    registerRoute(
+        ({url}) => url.href.startsWith('http'),
+        new StaleWhileRevalidate()
+    );
